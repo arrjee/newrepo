2b701aad900d0ca3b17ec1e9174ed2f5862725cd
Raju Singh <arrjee@gmail.com>
2021-04-25 09:22:11 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export GIT_COMMITTER_DATE="Wed Apr 11 12:01 2020 +0100"
+export GIT_AUTHOR_DATE="Wed Apr 11 12:09 2021 +0100"
+git add .
+git commit -am " new update to code scripts"
+git push origin master
